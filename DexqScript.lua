e2e93a07118167c5c568f5229282f701c5d33efc
tatate034-byte <tatate034@gmail.com>
2026-08-08 22:04:03 +0700

    Update DexqScript.lua

--- DexqScript.lua
+++ DexqScript.lua
@@ -119,7 +119,7 @@ local RaritiesList = {
     "Manga", "Celestial", "Heavenly", "Corrupted", "Striker", "Sacred", "Paradox", 
     "Founder", "Evolved", "Magic", "Oni", "Chaos", "Ruin", "Reborn", "Beast", 
     "Nordic", "Hunter", "Soul", "Swordsman", "Gamer", "Revenge", "Chainsaw", 
-    "Eternity", "Academy", "Dynasty", "Grail", "Mystery", "VIP", "Event", "Limited"
+    "Eternity", "Academy", "Dynasty", "Grail", "Mystery", "VIP", "Event", "Limited", "Raven", "Arcane", "Nightfall"
 }
 
 getgenv().SelectedRarities = {}
